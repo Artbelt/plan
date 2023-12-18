190e97e5cc72c6f31b6259f6b5f1bd90d41749a1
Belchikov <artbelt.pooh@gmail.com>
2023-12-18 13:06:16 +0200

    Initial commit

---
+++ add_panel_filter_into_db.php
@@ -0,0 +1,122 @@
+<?php
+require_once('tools/tools.php');
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+    <title>Добавление нового панельного фильтра в БД</title>
+    <style>
+        article, aside, details, figcaption, figure, footer,header,
+        hgroup, menu, nav, section { display: block; }
+    </style>
+</head>
+<body>
+
+
+<?php  echo '<table border="1"><tr><td>'.$_POST['workshop'].'</td></tr></table>';
+if (isset($_POST['filter_name'])){
+    $filter_name = $_POST['filter_name'];
+} else {
+    $filter_name = '';
+}
+
+if (isset($_POST['analog_filter']) AND ($_POST['analog_filter'] != '')){
+    $analog_filter = $_POST['analog_filter'];
+    /** Если аналог установлен то загружаем всю информацию в поля о аналоге */
+    echo "<p>ANALOG_FILTER=".$analog_filter;
+    // массив для записи всех значений аналога
+    $analog_data = get_filter_data($analog_filter);
+    //var_dump(get_filter_data($analog_filter));
+
+}else{
+    echo "<p> Аналог не определен";
+    $analog_data = array();
+    $analog_data['paper_package_length'] ='';
+    $analog_data['paper_package_width'] ='';
+    $analog_data['paper_package_height'] ='';
+    $analog_data['paper_package_pleats_count'] ='';
+    $analog_data['paper_package_amplifier'] ='';
+    $analog_data['paper_package_remark'] ='';
+    $analog_data['paper_package_supplier'] ='';
+    $analog_data['wireframe_length'] ='';
+    $analog_data['wireframe_width'] ='';
+    $analog_data['wireframe_material'] ='';
+    $analog_data['wireframe_supplier'] ='';
+    $analog_data['prefilter_length'] ='';
+    $analog_data['prefilter_width'] ='';
+    $analog_data['prefilter_material'] ='';
+    $analog_data['prefilter_supplier'] ='';
+    $analog_data['prefilter_remark'] ='';
+    $analog_data['box'] ='';
+    $analog_data['g_box'] ='';
+    $analog_data['comment'] ='';
+}
+
+?>
+
+<form action="processing_add_panel_filter_into_db.php" method="post" >
+    <label><b>Наименование фильтра</b>
+    <input type="text" name="filter_name" size="40" value="<?php echo $filter_name?>"><p>
+    </label>
+    <div id="mark"></div>
+    <label>Категория
+    <select name="category">
+        <option>Панельный</option>
+    </select>
+    </label><br>
+    <hr>
+    <label><b>Гофропакет:</b></label><p>
+        <label>Длина: <input type="text" size="5" name="p_p_length" value="<?php echo $analog_data['paper_package_length'] ?>"></label>
+        <label>Ширина: <input type="text" size="5" name="p_p_width" value="<?php echo $analog_data['paper_package_width'] ?>"></label>
+        <label>Высота:<input type="text" size="5" name="p_p_height" value="<?php echo $analog_data['paper_package_height'] ?>"> </label>
+        <label>Кол-во ребер: <input type="text" size="5" name="p_p_pleats_count" value="<?php echo $analog_data['paper_package_pleats_count'] ?>"></label>
+        <label>Усилитель: <input type="text" size="2" name="p_p_amplifier" value="<?php echo $analog_data['paper_package_amplifier'] ?>"></label>
+        <label>Поставщик: <select name="p_p_supplier"  ><option></option>
+                                                        <option  <?php if ($analog_data['paper_package_supplier'] == 'У2'){echo 'selected';} ?> >У2</option>
+            </select></label><p>
+        <label>Комментарий: <input type="text" size="50" name="p_p_remark" value="<?php echo $analog_data['paper_package_remark'] ?>"></label><br>
+
+        <hr>
+    <label><b>Каркас</b></label><p>
+        <label>Длина: <input type="text" size="5" name="wf_length" value="<?php echo $analog_data['wireframe_length'] ?>"></label>
+        <label>Ширина: <input type="text" size="5" name="wf_width" value="<?php echo $analog_data['wireframe_width'] ?>"></label>
+        <label>Материал: <select name="wf_material"><option></option>
+                                                    <option <?php if ($analog_data['wireframe_material'] == 'ОЦ 0,45'){echo 'selected';} ?>>ОЦ 0,45</option>
+                                                    <option <?php if ($analog_data['wireframe_material'] == 'Жесть 0,22'){echo 'selected';} ?>>Жесть 0,22</option>
+                        </select></label>
+        <label>Поставщик: <select name="wf_supplier"><option></option>
+                                                    <option <?php if ($analog_data['wireframe_supplier'] == 'ЗУ'){echo 'selected';} ?>>ЗУ</option>
+                          </select><br></label>
+        <hr>
+
+    <label><b>Предфильтр</b></label><p>
+        <label>Длина:<input type="text" size="5" name="pf_length" value="<?php echo $analog_data['prefilter_length'] ?>"> </label>
+        <label>Ширина: <input type="text" size="5" name="pf_width"value="<?php echo $analog_data['prefilter_width'] ?>"></label>
+        <label>Материал:<select name="pf_material"><option></option>
+                                                    <option <?php if ($analog_data['prefilter_material'] == 'Н/т полотно'){echo 'selected';} ?>>Н/т полотно</option>
+                    </select> </label>
+        <label>Поставщик:<select name="pf_supplier"><option></option>
+                                                    <option <?php if ($analog_data['prefilter_supplier'] == 'УУ'){echo 'selected';} ?>>УУ</option>
+                        </select></label><p>
+        <label>Комментарий:<input type="text" size="50" name="pf_remark" value="<?php echo $analog_data['prefilter_remark'] ?>"></label><br>
+    <hr>
+    <label><b>Индивидуальная упаковка</b></label><p>
+        <label>Коробка №:   <select name="box"><?php select_boxes($analog_data['box']);?></select></label><br>
+    <hr>
+    <label><b>Групповая упаковка</b></label><p>
+        <label>Ящик №: <select name="g_box"><?php select_g_boxes($analog_data['g_box']);?></select></label><br>
+    <hr>
+    <label><b>Примечание</b>
+        <input type="text" size="100" name="remark" value="<?php echo $analog_data['comment'] ?>">
+    </label><p>
+    <hr>
+    <input type="submit" value="Сохранить фильтр">
+
+</form>
+
+</body>
+</html>
+
+
+
---
+++ processing_add_panel_filter_into_db.php
@@ -0,0 +1,86 @@
+<?php
+require_once('tools/tools.php');
+
+$filter_name =  $_POST['filter_name'];
+$category = $_POST['category'];
+/** ГОФРОПАКЕТ */
+$p_p_name = "гофропакет ".$filter_name;
+$p_p_length = $_POST['p_p_length'];
+$p_p_width = $_POST['p_p_width'];
+$p_p_height = $_POST['p_p_height'];
+$p_p_pleats_count = $_POST['p_p_pleats_count'];
+$p_p_amplifier = $_POST['p_p_amplifier'];
+$p_p_supplier = $_POST['p_p_supplier'];
+$p_p_remark = $_POST['p_p_remark'];
+/** КАРКАС */
+/** Если заполнены все поля каркаса то присваиваем ему имя */
+$wf_length = $_POST['wf_length'];
+$wf_width = $_POST['wf_width'];
+$wf_material = $_POST['wf_material'];
+$wf_supplier = $_POST['wf_supplier'];
+if (($wf_length != '') and ($wf_width != '') and ($wf_material != '') and ($wf_supplier != '')){
+
+    $wf_name = "каркас ".$filter_name;
+
+} else {
+    $wf_name = "";
+}
+/** ПРЕДФИЛЬТР */
+/**Если заполнены все поля предфильтра то присваиваем ему имя  */
+$pf_length = $_POST['pf_length'];
+$pf_width = $_POST['pf_width'];
+$pf_material = $_POST['pf_material'];
+$pf_supplier = $_POST['pf_supplier'];
+$pf_remark = $_POST['pf_remark'];
+if (($pf_length != '') and ($pf_width != '') and ($pf_material != '') and ($pf_supplier != '')){
+
+    $pf_name = "предфильтр ".$filter_name;
+
+} else {
+
+    $pf_name = "";
+
+}
+/** УПАКОВКА ИНД*/
+$box = $_POST['box'];
+/** УПАКОВКА ГР */
+$g_box = $_POST['g_box'];
+/** ПРИМЕЧАНИЕ */
+$remark = $_POST['remark'];
+
+$a = check_filter($_POST['filter_name']);
+
+/** Если фильтр уже есть в БД -> выход */
+if ($a > 0){
+    echo "Фильтр {$filter_name} уже есть в БД";
+    exit();
+}
+
+/** Если фильтра в БД такого нет -> начинаем запись */
+
+/** Запись информации о фильтре в БД */
+$sql = "INSERT INTO panel_filter_structure(filter, category, paper_package, wireframe, prefilter, box, g_box, comment) 
+        VALUES ('$filter_name','$category','$p_p_name','$wf_name','$pf_name','$box','$g_box','$remark');";
+$result = mysql_execute($sql);
+
+/** Запись информации о гофропакете в БД */
+$sql = "INSERT INTO paper_package_panel(p_p_name, p_p_length, p_p_height, p_p_width, p_p_pleats_count, p_p_amplifier, supplier, p_p_remark) 
+        VALUES('$p_p_name','$p_p_length','$p_p_height','$p_p_width','$p_p_pleats_count','$p_p_amplifier','$p_p_supplier','$p_p_remark');";
+$result = mysql_execute($sql);
+
+/** Запись информации о каркасе в БД если каркас указан */
+if ($wf_name != ''){
+    $sql = "INSERT INTO wireframe_panel(w_name, w_length, w_width, w_material, w_supplier) 
+        VALUES('$wf_name','$wf_length','$wf_width','$wf_material','$wf_supplier');";
+    $result = mysql_execute($sql);
+}
+
+/** Запись информации о предфильтре в БД если предфильтр указан*/
+if ($pf_name != ''){
+    $sql = "INSERT INTO prefilter_panel(p_name, p_length, p_width, p_material, p_supplier, p_remark)
+       
+        VALUES('$pf_name','$pf_length','$pf_width','$pf_material','$pf_supplier','$pf_remark')";
+    $result = mysql_execute($sql);
+}
+
+echo "Фильтр {$filter_name} успешно добавлен в БД";
